c474b90f50037e1ca3dd6f7b31d6ae2f5205636c
neo <chojs990222@gmail.com>
2024-01-27 12:15:32 +0900

    Add menu

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use eframe::egui::{self, CentralPanel, SidePanel};
+mod menu;
 
-#[derive(Default)]
-enum Menu {
-    #[default]
-    Home,
-    Settings,
-    About,
-}
+use eframe::{
+    egui::FontFamily::Proportional,
+    egui::TextStyle::{Body, Button, Heading, Monospace, Small},
+    egui::{self, CentralPanel},
+    epaint::FontId,
+};
+
+use menu::{Menu, MenuTrait};
 
 #[derive(Default)]
 struct MyApp {
@@ -15,23 +16,29 @@ struct MyApp {
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.vertical(|ui| {
-                if ui.button("Home").clicked() {
-                    self.current_menu = Menu::Home;
-                }
-                if ui.button("Settings").clicked() {
-                    self.current_menu = Menu::Settings;
-                }
-                if ui.button("About").clicked() {
-                    self.current_menu = Menu::About;
-                }
-            });
-        });
+        let mut style = (*ctx.style()).clone();
+        style.text_styles = [
+            (Heading, FontId::new(30.0, Proportional)),
+            (Body, FontId::new(18.0, Proportional)),
+            (Monospace, FontId::new(14.0, Proportional)),
+            (Button, FontId::new(14.0, Proportional)),
+            (Small, FontId::new(10.0, Proportional)),
+        ]
+        .into();
+
+        ctx.set_style(style);
+
+        self.init_menu(ctx);
 
         CentralPanel::default().show(ctx, |ui| match self.current_menu {
             Menu::Home => {
                 ui.label("Home");
+            }
+            Menu::ColorPicker => {
+                ui.label("ColorPicker");
+            }
+            Menu::Jwt => {
+                ui.label("JWT");
             }
             Menu::Settings => {
                 ui.label("Settings");
@@ -50,6 +57,7 @@ fn main() -> Result<(), eframe::Error> {
         viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
         ..Default::default()
     };
+
     eframe::run_native(
         "Dev tools",
         options,
---
+++ src/menu.rs
@@ -0,0 +1,41 @@
+use eframe::egui::{self, SidePanel};
+
+use crate::MyApp;
+
+#[derive(Default)]
+pub enum Menu {
+    #[default]
+    Home,
+    ColorPicker,
+    Jwt,
+    Settings,
+    About,
+}
+
+pub trait MenuTrait {
+    fn init_menu(&mut self, ctx: &egui::Context);
+}
+
+impl MenuTrait for MyApp {
+    fn init_menu(&mut self, ctx: &egui::Context) {
+        SidePanel::left("side_panel").show(ctx, |ui| {
+            ui.vertical(|ui| {
+                if ui.button("Home").clicked() {
+                    self.current_menu = Menu::Home;
+                }
+                if ui.button("ColorPicker").clicked() {
+                    self.current_menu = Menu::ColorPicker;
+                }
+                if ui.button("JWT").clicked() {
+                    self.current_menu = Menu::Jwt;
+                }
+                if ui.button("Settings").clicked() {
+                    self.current_menu = Menu::Settings;
+                }
+                if ui.button("About").clicked() {
+                    self.current_menu = Menu::About;
+                }
+            });
+        });
+    }
+}
